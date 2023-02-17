a5edbe5e0fc2fff5ec4737ed3e1e9302affb3250
josuemj <98498772+josuemj@users.noreply.github.com>
2023-02-16 23:30:10 -0600

    Siglenton applied for Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    private static Calculator uniqueCalculator = null;
+
+    private Calculator(){} //Restricting the creation of the calculator
+
+    public static Calculator getCalculator() {
+        if (uniqueCalculator == null) {
+            uniqueCalculator = new Calculator();
+        }
+        return uniqueCalculator;
+    }
+
+    //Punch of methods for this calculator
+
+
+
+
+}
